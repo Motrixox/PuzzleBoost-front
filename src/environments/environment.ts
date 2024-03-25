081e55a0e76bb392d4e9274f74046ce766227fd2
Mateusz Cypcer <69849057+Motrixox@users.noreply.github.com>
2024-03-26 00:25:54 +0100

    updated api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseApiUrl: 'http://localhost:5216'
+    // baseApiUrl: 'http://localhost:5216'
+    baseApiUrl: 'https://sudokuwebservice.azurewebsites.net'
 };
